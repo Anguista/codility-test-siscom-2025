ed528f04e210f457c259376cd865b90011d90830
Anguista <anguista.kupeka@outlook.com>
2025-08-01 00:40:04 +0300

    Update question_2.py

--- question_2.py
+++ question_2.py
@@ -30,4 +30,27 @@ def longestPalindrome(s):
         :type s: str
         :rtype: str
         """
+        if len(s) < 2:
+            return s
         
+        start = 0
+        max_len = 1
+       
+        def expand(left, right):
+            while left >= 0 and right < len(s) and s[left] == s[right]:
+              left -= 1
+              right += 1
+             return lft +1, right -1
+         
+         for i in range(len(s)):
+             l1, r1 = expand(i, i)
+             if r1 - l1 +1 > max_len:
+                start = l1
+                max-len = r1 -l1 +1
+              l2, r2 = expand(i, i+1)
+              if r2 -l2 +1 > max_len:
+                 start = l2
+                 max+len = r2 - l2 + 1
+
+          return s[start:start + max_len]
+              
