cdd31022aaa2931bebe3d8b3839c3b6c3e77a155
Anguista <anguista.kupeka@outlook.com>
2025-08-01 01:22:59 +0300

    Update question_3.py

--- question_3.py
+++ question_3.py
@@ -63,4 +63,29 @@ def findSubstring(s, words):
         :type words: List[str]
         :rtype: List[int]
         """
+     if not s or not words:
+        return []
+      
+      word_len = len(words[0])
+      num_words = len(words)
+      total_len = word_len * num_words
+      word_count = Counter(words)
+      result = []
+
+      for i in range(len(s) - total_len +1:
+          seen = {}
+          for j in range(num_words):
+            word_start = i + j * word_len
+            word = s[word_start:word_start + word_len]
+            if word not in word_count:
+                break
+            seen[word] = seen.get(word, 0) + 1
+            if seen[word] > word_count[word]:
+                break
+        else:
+            result.append(i)
+
+    return result
+
+          
         
